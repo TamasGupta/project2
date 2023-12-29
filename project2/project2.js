bb04cdbcc256cf11811c5a7e116c0db66725675f
TamasGupta <thomasgupta9759@gmail.com>
2023-12-29 20:24:42 +0530

    todo list

---
+++ project2/project2.js
@@ -0,0 +1,40 @@
+let input = document.querySelector("input");
+// let li = document.querySelector("li");
+let btn = document.querySelector(".Add");
+let ul = document.querySelector("ul");
+
+
+
+
+function addTask(){
+
+
+ let item = document.createElement("li");
+ console.log(input.value)
+ item.innerText= input.value;
+
+ ul.appendChild(item);
+let delbtn = document.createElement("button");
+ delbtn.innerText="X";
+item.appendChild(delbtn)
+
+
+ input.value= "";
+
+
+}
+ 
+btn.addEventListener("click", ()=>{
+    addTask();
+})
+
+ul.addEventListener("click",(e)=>{
+console.dir(e.target.nodeName);
+if(e.target.nodeName=="BUTTON")
+{  
+  ul.removeChild(e.target.parentElement);
+    
+}
+
+
+})
